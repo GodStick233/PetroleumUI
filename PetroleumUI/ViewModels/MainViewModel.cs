5f4d3ff9f4231265f6233c0c6809a91ea9365f32
GodStick233 <499321348@qq.com>
2022-06-23 01:56:06 +0800

    update

---
+++ PetroleumUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using PetroleumUI.Common.Models;
+
+namespace PetroleumUI.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel()
+        {
+            MenuBars = new ObservableCollection<MenuBar>();
+            CreateMenuBar();
+        }
+        private ObservableCollection<MenuBar> menuBars;
+
+        public ObservableCollection<MenuBar> MenuBars
+        {
+            get { return menuBars; }
+            set { menuBars = value; RaisePropertyChanged(); }
+        }
+        
+        void CreateMenuBar()
+        {
+            MenuBars.Add(new MenuBar() { Icon = "Home", Title = "首页", NameSpace = "IndexView" });
+            MenuBars.Add(new MenuBar() { Icon = "Pipe", Title = "井筒设置", NameSpace = "WellSetView" });
+            MenuBars.Add(new MenuBar() { Icon = "ClipboardFlow", Title = "多相管流", NameSpace = "MFlowView" });
+            MenuBars.Add(new MenuBar() { Icon = "Cog", Title = "设置", NameSpace = "SettingView" });
+        }
+    }
+}
