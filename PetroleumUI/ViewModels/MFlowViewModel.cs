dff98312747bf8d573a99f8e4f4294ace79f996f
GodStick233 <499321348@qq.com>
2022-06-25 01:24:26 +0800

    test chart

--- PetroleumUI/ViewModels/MFlowViewModel.cs
+++ PetroleumUI/ViewModels/MFlowViewModel.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+using LiveChartsCore.Geo;
 namespace PetroleumUI.ViewModels
 {
-    class MFlowViewModel
+    public partial class MFlowViewModel
     {
+        public ISeries[] Series { get; set; } =
+        {
+        new LineSeries<double>
+        {
+            Values = new double[] { 5, 0, 5, 0, 5, 0 },
+            Fill = null,
+            GeometrySize = 0,
+            // use the line smoothness property to control the curve
+            // it goes from 0 to 1
+            // where 0 is a straight line and 1 the most curved
+            LineSmoothness = 0
+        },
+        new LineSeries<double>
+        {
+            Values = new double[] { 7, 2, 7, 2, 7, 2 },
+            Fill = null,
+            GeometrySize = 0,
+            LineSmoothness = 1
+        }
+        };
     }
+    
 }
